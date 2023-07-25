7ba2d66e7dac80f04857b748aa8a61769de08bdc
Marcos Alanya <asesoralanya19@gmail.com>
2023-07-25 15:14:55 -0500

    ADD: Se implemento sidebar completo con diseño

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -1,11 +1,11 @@
 import { config } from "@/config";
 import { PublicRoutes } from "@/models";
 import { resetUser } from "@/redux/state/userSlice";
-import { AppDispatch } from "@/redux/store";
+import { AppDispatch, AppStore } from "@/redux/store";
 import { clearLocalStorage } from "@/utils/localStorage.util";
 import React from "react";
-import { RiBriefcaseLine, RiCloseLine, RiDashboardLine, RiLogoutBoxRLine, RiMenu3Fill } from "react-icons/ri";
-import { useDispatch } from "react-redux";
+import { RiBriefcaseLine, RiCloseLine, RiDashboardLine, RiLogoutBoxRLine, RiMenu3Fill, RiSurveyLine, RiUser2Line, RiUser3Line } from "react-icons/ri";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 
 const optionsSidebar = [
@@ -21,6 +21,7 @@ const optionsSidebar = [
 
 export const Sidebar: React.FC = () => {
   const [sidebar, setSidebar] = React.useState(false);
+  const userState = useSelector((store: AppStore) => store.user)
 
   const handleSidebar = () => {
     setSidebar(!sidebar);
@@ -34,9 +35,9 @@ export const Sidebar: React.FC = () => {
         } h-full overflow-y-auto col-span-1 p-8 border-r`}
       >
         <div className="text-center p-8">
-          <h1 className="uppercase font-bold tracking-[4px]">DASHBOARD</h1>
+          <h1 className="uppercase font-bold tracking-[4px]">{userState.username}</h1>
         </div>
-        <div className="flex flex-col justify-between h-[800px]">
+        {/* <div className="flex flex-col justify-between h-[800px]">
           <nav>
             <ul>
               {optionsSidebar.map((opt) => (
@@ -50,7 +51,44 @@ export const Sidebar: React.FC = () => {
             </ul>
           </nav>
           <Reminder />
+        </div> */}
+
+        <div className="flex flex-col justify-between h-[800px]">
+          <nav>
+            <h5 className="uppercase font-bold text-xs text-primary tracking-[2px] mb-4">Registros</h5>
+            <ul>
+              <li>
+                <Link to={""} className="flex items-center gap-4 p-2 hover:bg-gray-200 transition-colors rounded-lg">
+                  <RiUser2Line className="text-[1.25rem]"/>
+                  <span>Profesores</span>
+                </Link>
+              </li>
+              <li>
+                <Link to={""} className="flex items-center gap-4 p-2 hover:bg-gray-200 transition-colors rounded-lg">
+                  <RiUser3Line className="text-[1.25rem]"/>
+                  <span>Alumnos</span>
+                </Link>
+              </li>
+              <li>
+                <Link to={""} className="flex items-center gap-4 p-2 hover:bg-gray-200 transition-colors rounded-lg">
+                  <RiSurveyLine className="text-[1.25rem]"/>
+                  <span>Materias</span>
+                </Link>
+              </li>
+            </ul>
+            <h5 className="uppercase font-bold text-xs text-primary tracking-[2px] my-4">Otros</h5>
+            <ul>
+              <li>
+                <Link to={""} className="flex items-center gap-4 p-2 hover:bg-gray-200 transition-colors rounded-lg">
+                  <RiSurveyLine className="text-[1.25rem]"/>
+                  <span>...Proximamente</span>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <Reminder />
         </div>
+
       </div>
       <button
         onClick={handleSidebar}
@@ -76,29 +114,17 @@ function Reminder() {
 
   return (
     <div className="flex flex-col gap-4">
-      <img src="./img/sidebar.svg" alt="Image" />
+      <img src="/img/sidebar.svg" alt="Image" />
       <div className="bg-primary bg-opacity-10 p-8 flex flex-col gap-4 rounded-3xl">
         <h3 className="text-xl text-center">RECORDATORIO</h3>
         <p className="text-gray-500 text-center">
           Toda informacion mostrada en las tablas es confidencial.
         </p>
       </div>
-      <button onClick={handleLogout} className="flex items-center gap-4 hover:bg-primary hover:bg-opacity-70 p-4 text-gray-400 hover:text-white rounded-lg transition-colors font-semibold">
+      <button onClick={handleLogout} className="flex items-center gap-4 hover:bg-primary hover:bg-opacity-80 p-4 hover:text-white rounded-lg transition-colors font-semibold">
         <RiLogoutBoxRLine/>
         Cerrar Sesión
       </button>
     </div>
-  )
-}
-
-function Option({...props}: {to: string, icon: JSX.Element, name: string}) {
-  return (
-    <Link
-      to={props.to}
-      className="flex items-center gap-4 hover:bg-primary hover:bg-opacity-70 p-4 text-gray-400 hover:text-white rounded-lg transition-colors font-semibold"
-    >
-      {props.icon}
-      {props.name}
-    </Link>
   )
 }
