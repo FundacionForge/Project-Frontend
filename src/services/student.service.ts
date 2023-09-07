f1e12aea2d3044d6f5ec0e8b65285595db188f89
Marcos <asesoralanya19@gmail.com>
2023-09-06 21:56:34 -0500

    ADD: Se implemento update de student

--- src/services/student.service.ts
+++ src/services/student.service.ts
@@ -1,7 +1,7 @@
-import { axiosClient } from "@/configs/axios.config";
-import { StudentDto } from "./dtos/student.dto";
-import { ResponseStudent, Student } from "./entities/student.entity";
+import { axiosClient } from '@/configs/axios.config';
 import { generateObjectId } from '../utils/generateObjectId';
+import { StudentDto } from './dtos/student.dto';
+import { ResponseStudent, Student } from './entities/student.entity';
 
 const endpoint = `/api/student`;
 
@@ -10,21 +10,26 @@ export const createStudent = async (student: StudentDto) => {
   student.degrees = generateObjectId(student.degrees);
   student.shifts = generateObjectId(student.shifts);
 
-  const { data } = await axiosClient.post<ResponseStudent>(`${endpoint}`, student)
+  const { data } = await axiosClient.post<ResponseStudent>(`${endpoint}`, student);
   return data;
-}
+};
+
+export const updateStudent = async (id: string, student: StudentDto) => {
+  const { data } = await axiosClient.put<ResponseStudent>(`${endpoint}/${id}`, student);
+  return data;
+};
 
 export const getAllStudent = async () => {
-  const { data } = await axiosClient.get<ResponseStudent>(`${endpoint}`)
+  const { data } = await axiosClient.get<ResponseStudent>(`${endpoint}`);
   return data;
-}
+};
 
 export const getStudent = async (id: string) => {
-  const { data } = await axiosClient.get<Student>(`${endpoint}/${id}`)
+  const { data } = await axiosClient.get<Student>(`${endpoint}/${id}`);
   return data;
-}
+};
 
 export const deleteStudent = async (id: string) => {
-  const { data } = await axiosClient.delete(`${endpoint}/${id}`)
+  const { data } = await axiosClient.delete(`${endpoint}/${id}`);
   return data;
-}
+};
